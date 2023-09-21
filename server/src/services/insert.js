463418fe484d738cd20cae5432e48724d30482a3
Chrugez <thanhcuis99@gmail.com>
2023-09-21 12:57:03 +0700

    #23 Create Model Province

--- server/src/services/insert.js
+++ server/src/services/insert.js
@@ -10,72 +10,103 @@ import { dataPrice, dataArea } from '../ultis/data'
 import { getNumberFromString } from '../ultis/common'
 require('dotenv').config()
 
-const dataBody = chothuematbang.body
+const dataBody = [
+    {
+        body: chothuephongtro.body,
+        code: 'CTPT'
+    },
+    {
+        body: chothuematbang.body,
+        code: 'CTMB'
+    },
+    {
+        body: chothuecanho.body,
+        code: 'CTCH'
+    },
+    {
+        body: nhachothue.body,
+        code: 'NCT'
+    },
+]
 
 const hashPassword = password => bcrypt.hashSync(password, bcrypt.genSaltSync(12))
 
 export const insertService = () => new Promise(async (resolve, reject) => {
     try {
-        dataBody.forEach(async (item) => {
-            let postId = v4()
-            let labelCode = generateCode(item?.header?.class?.classType)
-            let attributeId = v4()
-            let userId = v4()
-            let imagesId = v4()
-            let overviewId = v4()
-            let desc = JSON.stringify(item?.mainContent?.content)
-            let currentArea = getNumberFromString(item?.header?.attributes?.acreage)
-            let currentPrice = getNumberFromString(item?.header?.attributes?.price)
-            await db.Post.create({
-                id: postId,
-                title: item?.header?.title,
-                star: item?.header?.star,
-                labelCode,
-                address: item?.header?.address,
-                attributeId,
-                categoryCode: 'CTMB',
-                description: desc,
-                userId,
-                overviewId,
-                imagesId,
-                areaCode: dataArea.find(area => area.max > currentArea && area.min <= currentArea)?.code,
-                priceCode: dataPrice.find(price => price.max > currentPrice && price.min <= currentPrice)?.code,
-            })
-            await db.Attribute.create({
-                id: attributeId,
-                price: item?.header?.attributes?.price,
-                acreage: item?.header?.attributes?.acreage,
-                published: item?.header?.attributes?.published,
-                hashtag: item?.header?.attributes?.hashtag,
-            })
-            await db.Image.create({
-                id: imagesId,
-                image: JSON.stringify(item?.images)
-            })
-            await db.Label.findOrCreate({
-                where: { code: labelCode },
-                defaults: {
+        const provinceCodes = []
+        const labelCodes = []
+        dataBody.forEach(cate => {
+            cate.body.forEach(async (item) => {
+                let postId = v4()
+                let labelCode = generateCode(item?.header?.class?.classType).trim()
+                labelCodes?.every(item => item?.code !== labelCode) && labelCodes.push({
                     code: labelCode,
-                    value: item?.header?.class?.classType
-                }
+                    value: item?.header?.class?.classType?.trim()
+                })
+                let provinceCode = generateCode(item?.header?.address?.split(',')?.slice(-1)[0]).trim()
+                provinceCodes?.every(item => item?.code !== provinceCode) && provinceCodes.push({
+                    code: provinceCode,
+                    value: item?.header?.address?.split(',')?.slice(-1)[0].trim()
+                })
+                let attributeId = v4()
+                let userId = v4()
+                let imagesId = v4()
+                let overviewId = v4()
+                let desc = JSON.stringify(item?.mainContent?.content)
+                let currentArea = getNumberFromString(item?.header?.attributes?.acreage)
+                let currentPrice = getNumberFromString(item?.header?.attributes?.price)
+                await db.Post.create({
+                    id: postId,
+                    title: item?.header?.title,
+                    star: item?.header?.star,
+                    labelCode,
+                    address: item?.header?.address,
+                    attributeId,
+                    categoryCode: cate.code,
+                    description: desc,
+                    userId,
+                    overviewId,
+                    imagesId,
+                    areaCode: dataArea.find(area => area.max > currentArea && area.min <= currentArea)?.code,
+                    priceCode: dataPrice.find(price => price.max > currentPrice && price.min <= currentPrice)?.code,
+                    provinceCode
+                })
+                await db.Attribute.create({
+                    id: attributeId,
+                    price: item?.header?.attributes?.price,
+                    acreage: item?.header?.attributes?.acreage,
+                    published: item?.header?.attributes?.published,
+                    hashtag: item?.header?.attributes?.hashtag,
+                })
+                await db.Image.create({
+                    id: imagesId,
+                    image: JSON.stringify(item?.images)
+                })
+                await db.Overview.create({
+                    id: overviewId,
+                    code: item?.overview?.content.find(i => i.name === "Mã tin:")?.content,
+                    area: item?.overview?.content.find(i => i.name === "Khu vực")?.content,
+                    type: item?.overview?.content.find(i => i.name === "Loại tin rao:")?.content,
+                    target: item?.overview?.content.find(i => i.name === "Đối tượng thuê:")?.content,
+                    bonus: item?.overview?.content.find(i => i.name === "Gói tin:")?.content,
+                    created: item?.overview?.content.find(i => i.name === "Ngày đăng:")?.content,
+                    expired: item?.overview?.content.find(i => i.name === "Ngày hết hạn:")?.content
+                })
+                await db.User.create({
+                    id: userId,
+                    name: item?.contact?.content.find(i => i.name === "Liên hệ:")?.content,
+                    password: hashPassword('123456'),
+                    phone: item?.contact?.content.find(i => i.name === "Điện thoại:")?.content,
+                    zalo: item?.contact?.content.find(i => i.name === "Zalo")?.content,
+                })
             })
-            await db.Overview.create({
-                id: overviewId,
-                code: item?.overview?.content.find(i => i.name === "Mã tin:")?.content,
-                area: item?.overview?.content.find(i => i.name === "Khu vực")?.content,
-                type: item?.overview?.content.find(i => i.name === "Loại tin rao:")?.content,
-                target: item?.overview?.content.find(i => i.name === "Đối tượng thuê:")?.content,
-                bonus: item?.overview?.content.find(i => i.name === "Gói tin:")?.content,
-                created: item?.overview?.content.find(i => i.name === "Ngày đăng:")?.content,
-                expired: item?.overview?.content.find(i => i.name === "Ngày hết hạn:")?.content
-            })
-            await db.User.create({
-                id: userId,
-                name: item?.contact?.content.find(i => i.name === "Liên hệ:")?.content,
-                password: hashPassword('123456'),
-                phone: item?.contact?.content.find(i => i.name === "Điện thoại:")?.content,
-                zalo: item?.contact?.content.find(i => i.name === "Zalo")?.content,
-            })
+        })
+
+        provinceCodes?.forEach(async (item) => {
+            await db.Province.create(item)
+        })
+        labelCodes?.forEach(async (item) => {
+            await db.Label.create(item)
         })
 
 
